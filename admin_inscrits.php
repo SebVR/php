350c744737c417db364dbdbf1ee1b28e8657639b
Cyrille Flamant <cyrille.flamant@gmail.com>
2026-03-30 10:53:44 +0200

    eval php commit final
    Question 1 et Question 2 Partie A réalisées

--- admin_inscrits.php
+++ admin_inscrits.php
@@ -13,14 +13,15 @@ $fichier = fopen("inscrits.txt", "r");
     $tableau = [];
     while (!feof($fichier)) {
         $ligne = fgets($fichier);
-        explode('|', $ligne);
-        array_push ($tableau, $ligne);
+        $ligneExplosee = explode('|', $ligne);
+        array_push ($tableau, $ligneExplosee);
     }
+
     $ligneHTML = "";
-    for($i=0;$i<count($tableau); $i++) {
-        $ligneHTML = $ligneHTML . '<tr><td>' . $tableau[$i] . '</td><td>' . $tableau[$i] . '</td><td>' . $tableau[$i] . '</td><td>' . $tableau[$i];
+    for($i=0;$i<count($tableau)-1; $i++) {
+        $ligneHTML = $ligneHTML . '<tr><td>' . $tableau[$i][0] . '</td><td>' . $tableau[$i][1] . '</td><td>' . $tableau[$i][2] . '</td><td>' . $tableau[$i][3] . '</td></tr>';
     }
-    $tableauHTML = '<table><tr><th>Prénom</th><th>Email</th><th>Niveau</th><th>Date d\'inscription</th></tr></table>' . $ligneHTML;
+    $tableauHTML = '<table><tr><th>Prénom</th><th>Email</th><th>Niveau</th><th>Date d\'inscription' . $ligneHTML . '</th></tr></table>';
     echo $tableauHTML;
     }
 ?>
